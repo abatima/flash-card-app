f14d47b98e3a490a27ab99de108cc84b507dd40c
abatima <renat.kulalov.work@gmail.com>
2026-02-19 10:03:05 +0100

    added word picking from csv

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from tkinter import *
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
+
+def get_new_word():
+    data = pandas.read_csv("data/french_words.csv")
+    foreign_words = data.French.to_list()
+    random_foreign_word = random.choice(foreign_words)
+    canvas.itemconfig(foreign_word_canvas_text, text= random_foreign_word)
 
 window = Tk()
 window.title("Flash Card App")
@@ -10,21 +18,16 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263, image= card_front)
 canvas.create_text(400, 143, text="French", font=(FONT_NAME, 40, "italic"))
-canvas.create_text(400, 303, text="Trouve", font=(FONT_NAME, 60, "bold"))
+foreign_word_canvas_text = canvas.create_text(400, 303, text="Trouve", font=(FONT_NAME, 60, "bold"))
 canvas.grid(column=1,row=0,columnspan=2)
 
-# learning_language_label = Label(text = "French", font=(FONT_NAME, 40, "italic"))
-# learning_language_label.grid(column=1,row=0)
-#
-# data_language_word_label = Label(text = "Timer", font=(FONT_NAME, 60, "bold"))
-# data_language_word_label.grid(column=1,row=0)
 
 wrong_button_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_button_image, highlightthickness=0)
+wrong_button = Button(image=wrong_button_image, command=get_new_word, highlightthickness=0)
 wrong_button.grid(column=1,row=1)
 
 right_button_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_image, highlightthickness=0)
+right_button = Button(image=right_button_image, command=get_new_word, highlightthickness=0)
 right_button.grid(column=2,row=1)
 
 window.mainloop()
