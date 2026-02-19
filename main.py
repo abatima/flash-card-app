5def0e1bc2a7cbbd3df3c891119f95c05094246a
abatima <renat.kulalov.work@gmail.com>
2026-02-19 09:52:09 +0100

    removed window dimensions and renamed buttons

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ FONT_NAME = "Arial"
 
 window = Tk()
 window.title("Flash Card App")
-window.config(width=800, height=800, padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
@@ -19,12 +19,12 @@ canvas.grid(column=1,row=0,columnspan=2)
 # data_language_word_label = Label(text = "Timer", font=(FONT_NAME, 60, "bold"))
 # data_language_word_label.grid(column=1,row=0)
 
-wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_button_image, highlightthickness=0)
 wrong_button.grid(column=1,row=1)
 
-right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_button_image, highlightthickness=0)
 right_button.grid(column=2,row=1)
 
 window.mainloop()
